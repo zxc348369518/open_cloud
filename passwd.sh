e6059779daba82ee7666d6b9a849cce1c71d402d
Leige <32031806+LG-leige@users.noreply.github.com>
2022-10-02 07:24:56 +0800

    Update passwd.sh

--- passwd.sh
+++ passwd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo root:hK*6n%Nd%PiLk&$p |sudo chpasswd root
+echo root:GVuRxZYMiOwgdiTd |sudo chpasswd root
 sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
 sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
 sudo service sshd restart
