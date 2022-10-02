e16f6f7b1e347fd0903e9ed44c52afffb17ac787
Leige <32031806+LG-leige@users.noreply.github.com>
2022-10-02 15:45:33 +0800

    Add files via upload

--- opencloud.sh
+++ opencloud.sh
@@ -341,7 +341,38 @@ del_api_do(){
 
 #初始化
 initialization(){
-    
+    depends=("jq")
+    depend=""
+    for i in "${!depends[@]}"; do
+      now_depend="${depends[$i]}"
+      if [ ! -x "$(command -v $now_depend 2>/dev/null)" ]; then
+        echo "$now_depend 未安装"
+        depend="$now_depend $depend"
+      fi
+    done
+    if [ "$depend" ]; then
+      if [ -x "$(command -v apk 2>/dev/null)" ]; then
+        echo "apk包管理器,正在尝试安装依赖:$depend"
+        apk --no-cache add $depend $proxy >>/dev/null 2>&1
+      elif [ -x "$(command -v apt-get 2>/dev/null)" ]; then
+        echo "apt-get包管理器,正在尝试安装依赖:$depend"
+        apt -y install $depend >>/dev/null 2>&1
+      elif [ -x "$(command -v yum 2>/dev/null)" ]; then
+        echo "yum包管理器,正在尝试安装依赖:$depend"
+        yum -y install $depend >>/dev/null 2>&1
+      else
+        red "未找到合适的包管理工具,请手动安装:$depend"
+        exit 1
+      fi
+      for i in "${!depends[@]}"; do
+        now_depend="${depends[$i]}"
+        if [ ! -x "$(command -v $now_depend)" ]; then
+          red "$now_depend 未成功安装,请尝试手动安装!"
+          exit 1
+        fi
+      done
+    fi
+
     mkdir -p /root/opencloud
     mkdir -p /root/opencloud/do
     mkdir -p /root/opencloud/linode
@@ -367,7 +398,7 @@ read -p " 请输入数字 :" num
     digitalocean_menu
     ;;
     2)
-    create_linode
+    linode_menu
     ;;
     99)
     exit 1
