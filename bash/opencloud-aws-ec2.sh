8cf1c6d89cd300bfc915fcbba3dcbe59d3fd6990
Leige <32031806+LG-leige@users.noreply.github.com>
2022-10-13 16:10:02 +0800

    update

--- bash/opencloud-aws-ec2.sh
+++ bash/opencloud-aws-ec2.sh
@@ -154,7 +154,7 @@ create_win_aws_EC2(){
     --security-group-ids ${sgid} \
     --subnet-id ${SubnetId} \
     --region ${region} \
-    --block-device-mappings "[{\"DeviceName\":\"/dev/xvda\",\"Ebs\":{\"VolumeSize\":${vds},\"DeleteOnTermination\":true}}]"`
+    --block-device-mappings "[{\"DeviceName\":\"/dev/xvda\",\"Ebs\":{\"VolumeSize\":${vda},\"DeleteOnTermination\":true}}]"`
     
     InstanceId=`echo $json | jq -r '.Instances[0].InstanceId'`
     
@@ -190,7 +190,7 @@ sudo service sshd restart;
     --security-group-ids ${sgid} \
     --subnet-id ${SubnetId} \
     --region ${region} \
-    --block-device-mappings "[{\"DeviceName\":\"/dev/xvda\",\"Ebs\":{\"VolumeSize\":${vds},\"DeleteOnTermination\":true}}]"`
+    --block-device-mappings "[{\"DeviceName\":\"/dev/xvda\",\"Ebs\":{\"VolumeSize\":${vda},\"DeleteOnTermination\":true}}]"`
     
     InstanceId=`echo $json | jq -r '.Instances[0].InstanceId'`
     
