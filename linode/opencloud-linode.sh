d547390b71773b6278f28ee44a3c527087f2abbc
Leige <leige966@gmail.com>
2022-12-14 08:42:58 +0800

    update

--- linode/opencloud-linode.sh
+++ linode/opencloud-linode.sh
@@ -8,7 +8,7 @@ Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[信息]${Font_color_suffix}"
 Error="${Red_font_prefix}[错误]${Font_color_suffix}"
 Tip="${Green_font_prefix}[注意]${Font_color_suffix}"
-file_path="/root/opencloud/linode"
+file_path="/root/.opencloud/linode"
 #linode循环脚本
 linode_loop_script(){
 echo -e "
